3cd26ac83527ec1396978b0934d57d976233e147
whutwei <twei@whu.edu.cn>
2018-03-09 00:19:55 +0800

    V2版本，不卡死但存在断开后无法再次连接bug

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ DJI 入职作业
 ## 说明（基于树莓派的远程智能相机）
 树莓派采集视频，由后台GPU服务器进行目标检测。用户打开浏览器访问远程视频及检测结果。
 
-本分支处理树莓派端的视频采集，和H.264流的生成
+本分支处理树莓派端的视频采集，和H.264流的生成，并实现RTSP服务器推流
 
 ### 树莓派:
-1) 获取USB摄像头数据，直接采用opencv（本质上基于V4L2）。
+1) 获取USB摄像头数据，直接采用opencv（基于V4L2）。
 2) 视频流编码格式采用H.264，基于x264库进行编码。
+3) RTSP推流，可用VLC实现播放
 ### Ubuntu PC:
 1) H.264视频解码。
 2) 基于YOLO网络检测视频中的目标。
@@ -21,10 +22,11 @@ DJI 入职作业
 
 ## 依赖项
 ### 树莓派:
-libcv-dev x264
+libcv-dev x264 live555
 
 libcv-dev的安装：sudo apt-get install libcv-dev；
 x264的安装：采用源码编译安装，参考https://www.jianshu.com/p/dec9bf9cffc9
+live555的编译后请运行sudo make install，自动安装至/usr/local/lib目录下
 
 ### Ubuntu PC:
 CUDA-8.0 CUDNN OpenBLAS OpenCV3.3.1 CMake FFmpeg
@@ -43,6 +45,7 @@ make
 cd ./build
 bin/main
 ```
+VLC播放器打开后，点击播放按钮-选择网络选项-输入网络URL：rtsp://192.168.1.102:8554/testStream，即可。
 
 ## Reference
 https://www.jianshu.com/p/dec9bf9cffc9
--- src/main.cpp
+++ src/main.cpp
@@ -4,50 +4,54 @@
 #include <fstream>
 #include "x264_encoder.h"
 
-#include <liveMedia.hh>
-#include <BasicUsageEnvironment.hh>
-#include <GroupsockHelper.hh>
-#include <sys/types.h>
-#include <sys/stat.h>
-
 using namespace std;
 using std::ofstream;
 using namespace cv;
 
+#include "liveMedia.hh"
+#include "BasicUsageEnvironment.hh"
+#include <GroupsockHelper.hh>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 UsageEnvironment* env;
-char const* inputFileName = "test.264";
-H264VideoStreamFramer* videoSource;
-RTPSink* videoSink;
 
-void play(); // forward
+// To make the second and subsequent client for each stream reuse the same
+// input stream as the first client (rather than playing the file from the
+// start for each client), change the following "False" to "True":
+Boolean reuseFirstSource = False;
 
-EventTriggerId DeviceSource::eventTriggerId = 0;
-VideoCapture cap;
+// To stream *only* MPEG-1 or 2 video "I" frames
+// (e.g., to reduce network bandwidth),
+// change the following "False" to "True":
+Boolean iFramesOnly = False;
+
+static void announceStream(RTSPServer* rtspServer, ServerMediaSession* sms,
+			   char const* streamName, char const* inputFileName); // fwd
 
 int main(int argc, char** argv) {
   // Begin by setting up our usage environment:
   TaskScheduler* scheduler = BasicTaskScheduler::createNew();
   env = BasicUsageEnvironment::createNew(*scheduler);
 
-  // Create 'groupsocks' for RTP and RTCP:
-  struct in_addr destinationAddress;
-  destinationAddress.s_addr = chooseRandomIPv4SSMAddress(*env);
-  // Note: This is a multicast address.  If you wish instead to stream
-  // using unicast, then you should use the "testOnDemandRTSPServer"
-  // test program - not this test program - as a model.
+  UserAuthenticationDatabase* authDB = NULL;
+#ifdef ACCESS_CONTROL
+  // To implement client access control to the RTSP server, do the following:
+  authDB = new UserAuthenticationDatabase;
+  authDB->addUserRecord("username1", "password1"); // replace these with real strings
+  // Repeat the above with each <username>, <password> that you wish to allow
+  // access to the server.
+#endif
+  char const* streamName = "H264live";
+  //char const* inputFileName = "out.h264";
+	char const* inputFileName = "fifo.264";
 
-  const unsigned short rtpPortNum = 18888;
-  const unsigned short rtcpPortNum = rtpPortNum+1;
-  const unsigned char ttl = 255;
-
-  const Port rtpPort(rtpPortNum);
-  const Port rtcpPort(rtcpPortNum);
-
-	//摄像头部分
+  //摄像头部分
 	uint8_t* buf;
 	int bufsize;
 	Mat frame1;
 	x264Encoder x264;
+  VideoCapture cap;
 
 	cap.open(0);
 	cap.set(CV_CAP_PROP_FPS,10);
@@ -69,7 +73,7 @@ int main(int argc, char** argv) {
 				if(cap.isOpened())
 				{
 					cap>>frame1;
-					imshow("FRAME1:",frame1);
+					//imshow("FRAME1:",frame1);
 					bufsize = x264.EncodeOneFrame(frame1);
 					if(bufsize>0){
 						buf = x264.GetEncodedFrame();
@@ -85,129 +89,47 @@ int main(int argc, char** argv) {
     }
   }
 
-  Groupsock rtpGroupsock(*env, destinationAddress, rtpPort, ttl);
-  rtpGroupsock.multicastSendOnly(); // we're a SSM source
-  Groupsock rtcpGroupsock(*env, destinationAddress, rtcpPort, ttl);
-  rtcpGroupsock.multicastSendOnly(); // we're a SSM source
-
-
-
-  // Create a 'H264 Video RTP' sink from the RTP 'groupsock':
-  OutPacketBuffer::maxSize = 600000;
-  videoSink = H264VideoRTPSink::createNew(*env, &rtpGroupsock, 96);
-
-  // Create (and start) a 'RTCP instance' for this RTP sink:
-  const unsigned estimatedSessionBandwidth = 10000; // in kbps; for RTCP b/w share
-  const unsigned maxCNAMElen = 100;
-  unsigned char CNAME[maxCNAMElen+1];
-  gethostname((char*)CNAME, maxCNAMElen);
-  CNAME[maxCNAMElen] = '\0'; // just in case
-  RTCPInstance* rtcp
-  = RTCPInstance::createNew(*env, &rtcpGroupsock,
-                estimatedSessionBandwidth, CNAME,
-                videoSink, NULL /* we're a server */,
-                True /* we're a SSM source */);
-  // Note: This starts RTCP running automatically
-
-  RTSPServer* rtspServer = RTSPServer::createNew(*env, 8554);
+  // Create the RTSP server:
+  RTSPServer* rtspServer = RTSPServer::createNew(*env, 8554, authDB);
   if (rtspServer == NULL) {
     *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
     exit(1);
   }
+
+  char const* descriptionString
+    = "Session streamed by \"testOnDemandRTSPServer\"";
+
   ServerMediaSession* sms
-    = ServerMediaSession::createNew(*env, "testStream", inputFileName,
-           "Session streamed by \"testH264VideoStreamer\"",
-                       True /*SSM*/);
-  sms->addSubsession(PassiveServerMediaSubsession::createNew(*videoSink, rtcp));
+    = ServerMediaSession::createNew(*env, streamName, streamName,
+			      descriptionString);
+  sms->addSubsession(H264VideoFileServerMediaSubsession
+	       ::createNew(*env, inputFileName, reuseFirstSource));
   rtspServer->addServerMediaSession(sms);
 
-  char* url = rtspServer->rtspURL(sms);
-  *env << "Play this stream using the URL \"" << url << "\"\n";
-  delete[] url;
+  announceStream(rtspServer, sms, streamName, inputFileName);
 
-  // Start the streaming:
-  *env << "Beginning streaming...\n";
-  play();
+
+  // Also, attempt to create a HTTP server for RTSP-over-HTTP tunneling.
+  // Try first with the default HTTP port (80), and then with the alternative HTTP
+  // port numbers (8000 and 8080).
+
+  if (rtspServer->setUpTunnelingOverHTTP(80) || rtspServer->setUpTunnelingOverHTTP(8000) || rtspServer->setUpTunnelingOverHTTP(8080)) {
+    *env << "\n(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling.)\n";
+  } else {
+    *env << "\n(RTSP-over-HTTP tunneling is not available.)\n";
+  }
 
   env->taskScheduler().doEventLoop(); // does not return
 
   return 0; // only to prevent compiler warning
 }
 
-void afterPlaying(void* /*clientData*/) {
-  *env << "...done reading from file\n";
-  videoSink->stopPlaying();
-  Medium::close(videoSource);
-  cap.release();
-  // Note that this also closes the input file that this source read from.
-
-  // Start playing once again:
-  play();
+static void announceStream(RTSPServer* rtspServer, ServerMediaSession* sms,
+			   char const* streamName, char const* inputFileName) {
+  char* url = rtspServer->rtspURL(sms);
+  UsageEnvironment& env = rtspServer->envir();
+  env << "\n\"" << streamName << "\" stream, from the file \""
+      << inputFileName << "\"\n";
+  env << "Play this stream using the URL \"" << url << "\"\n";
+  delete[] url;
 }
-
-void play() {
-  // Open the input file as a 'byte-stream file source':
-  ByteStreamFileSource* fileSource
-    = ByteStreamFileSource::createNew(*env, inputFileName);
-  if (fileSource == NULL) {
-    *env << "Unable to open file \"" << inputFileName
-         << "\" as a byte-stream file source\n";
-    exit(1);
-  }
-
-  FramedSource* videoES = fileSource;
-
-  // Create a framer for the Video Elementary Stream:
-  videoSource = H264VideoStreamFramer::createNew(*env, videoES);
-
-  // Finally, start playing:
-  *env << "Beginning to read from file...\n";
-  videoSink->startPlaying(*videoSource, afterPlaying, videoSink);
-}
-
-// int main()
-// {
-// 	double t;
-// 	double fps;
-// 	uint8_t* buf;
-// 	int bufsize;
-// 	int count = 0;
-// 	Mat frame1;
-// 	x264Encoder x264;
-//
-// 	ofstream outfile("../out.h264",ios::app|ios::binary);
-//
-// 	VideoCapture cap(0);
-// 	cap.set(CV_CAP_PROP_FPS,10);
-// 	x264.Create(640,480,3,10);
-//
-// 	while(1)
-// 	{
-// 		if(waitKey(20)==27)break;
-// 		//t = (double)getTickCount();
-// 		if(cap.isOpened())
-// 		{
-// 			cap>>frame1;
-// 			//t=((double)getTickCount()-t)/getTickFrequency();
-// 			//fps = 1.0/t;
-// 			//cout<<fps<<endl;
-// 			imshow("FRAME1:",frame1);
-// 			bufsize = x264.EncodeOneFrame(frame1);
-// 			cout<<bufsize<<endl;
-// 			if(bufsize>0){
-// 				buf = x264.GetEncodedFrame();
-// 				outfile.write((char *)buf,bufsize);
-// 			}
-// 			if(++count>500)break;
-// 		}
-// 		else
-// 		{
-// 			cout<<"No camera Input!"<<endl;
-// 			outfile.close();
-// 			return -1;
-// 		}
-// 	}
-// 	outfile.close();
-// 	cout<<"finish~"<<endl;
-// 	return 0;
-// }
